756cba857d8406e07346e359f5cc28fa39d6a26b
vals25-mg <rvalisoa3@gmail.com>
2023-02-06 17:04:26 +0300

    add String and int

--- DeconnecterServlet.java
+++ DeconnecterServlet.java
@@ -5,5 +5,7 @@ public class DeconnecterServlet {
 
     public static void main(String[] args) {
         System.out.println("you are not connected");
+        int z=12;
+        String team="Alefa barea";
     }
 }
